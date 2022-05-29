b1a6fc8e237b7ad617688cef69883c9a8858176b
vladzabaluev <vladzabaluev014@gmail.com>
2022-05-29 18:49:09 +0300

    edit add

--- DailyApplication/DailyApplication/Controllers/EventsController.cs
+++ DailyApplication/DailyApplication/Controllers/EventsController.cs
@@ -24,12 +24,14 @@ namespace DailyApplication.Controllers
             _context = context;
             _userManager = userManager;
         }
+
         #region Создать ивент
+
         //Созда событие(сделать асинхронным)
         public Event CreateEvent(string Name, string Description, ClaimsPrincipal User, DateTime DeadlineTime, List<string> subEvents)
         {
             List<Sub_event> tempSubEv = new List<Sub_event>();
-            foreach(string descValue in subEvents)
+            foreach (string descValue in subEvents)
             {
                 Sub_event createdSubEvent = new Sub_event()
                 {
@@ -45,7 +47,7 @@ namespace DailyApplication.Controllers
                 Description = Description,
                 User = _userManager.GetUserAsync(User).Result,
                 DeadlineTime = DeadlineTime,
-                
+
                 SubEvents = tempSubEv,
                 IsDone = false
             };
@@ -54,63 +56,15 @@ namespace DailyApplication.Controllers
             return newEvent;
         }
 
-        //[Authorize]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //POST: Create
-        //public async Task<IActionResult> CreateEvent(string Name, string Description, ClaimsPrincipal User, DateTime DeadlineTime)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        Event newEvent = new Event()
-        //        {
-        //            Name = Name,
-        //            Description = Description,
-        //            User = _userManager.GetUserAsync(User).Result,
-        //            DeadlineTime = DeadlineTime,
-        //            IsDone = false
-        //        };
-        //        _context.Event.Add(newEvent);
-        //        await _context.SaveChangesAsync();
-        //        RedirectToPage(nameof(Events));
-        //    }
-        //    return new RedirectToPageResult(nameof(Events));
-        //    //return RedirectToPage("Events");
-
-        //}
-
-        //// GET: Events/Create
-        //[Authorize]
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Events/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[Authorize]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,Name,Description,DeadlineTime")] Event @event)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        @event.User = _userManager.GetUserAsync(User).Result;
-        //        @event.IsDone = false;
-        //        _context.Add(@event);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(@event);
-        //}
-        #endregion
+        #endregion Создать ивент
 
         #region Вернуть ивенты
+
         public List<Event> GetAllEvents()
         {
             return _context.Event.ToList();
         }
+
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -120,44 +74,11 @@ namespace DailyApplication.Controllers
             events.Sort((Event x, Event y) => x.DeadlineTime.CompareTo(y.DeadlineTime));
             return events;
         }
-        //public List<Event> GetUserEvents()
-        //{
-        //    return _context.Event.Where(ev => ev.User == _userManager.GetUserAsync(User).Result).ToList();
-        //}
-        // GET: Events
-        //public async Task<IActionResult> Index()
-        //{
-        //    return View(await _context.Event.ToListAsync());
-        //}
 
-        //[Authorize]
-        //public async Task<IActionResult> ShowUsersEvents()
-        //{
-        //    return View("Index", await _context.Event.
-        //        Where(ev => ev.User == _userManager.GetUserAsync(User).Result).ToListAsync());
-        //}
-
-        //public async Task<IActionResult> Prikol()
-        //{
-        //    return new RedirectToPageResult("Events", new { events = await _context.Event.ToListAsync() });
-        //}
-
-        // Чтение всех ивентов
-        //public IEnumerable<Event> Index()
-        //{
-        //    return _context.Event;
-        //}
-
-        //public async Task<> Index()
-        //{
-        //    return await _context.Event.ToArrayAsync();
-        //    //await _context.Event.ToListAsync();
-        //    //  return _context.Event.ToListAsync();
-        //    //   return RedirectToPage("Events");// (Pages.Events.Events, await _context.Event.ToListAsync());
-        //}
-        #endregion
+        #endregion Вернуть ивенты
 
         #region Получить подсобытия
+
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -167,9 +88,11 @@ namespace DailyApplication.Controllers
             List<Sub_event> curSubEvents = _context.Sub_event.Where(curSub => curSub.Event.Id == Id).ToList();
             return curSubEvents;
         }
-        #endregion
+
+        #endregion Получить подсобытия
 
         #region Удалить подсобытие
+
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -181,60 +104,12 @@ namespace DailyApplication.Controllers
                 return NotFound();
             }
             _context.Sub_event.Remove(removableSubEvent);
-            //List<Sub_event> removableSubEvents=GetEventSubEvents(removableEvent.Id);
-            //foreach(Sub_event sub_Event in removableSubEvents)
-            //{
-            //    Sub_event currentRemSubEvent = _context.Sub_event.FirstOrDefault(remSubEv => remSubEv.Id == sub_Event.Id);
-            //    _context.Sub_event.Remove(currentRemSubEvent);
-            //}
             return new RedirectToPageResult(nameof(GetUserEvents));
         }
-        #endregion
+
+        #endregion Удалить подсобытие
 
         #region Удалить события
-
-        // D
-        //public void DeleteEvent(int? id)
-        //{
-
-        //    var deletedItem = _context.Event.Find(Id);
-
-        //    if (deletedItem != null)
-        //    {
-        //        _context.Event.Remove(deletedItem);
-        //        _context.SaveChanges();
-        //    }
-        //}
-        //// GET: NewEvents/Delete/5
-        //[Authorize]
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var @event = await _context.Event
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (@event == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(@event);
-        //}
-
-        //// POST: NewEvents/Delete/5
-        //[Authorize]
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var @event = await _context.Event.FindAsync(id);
-        //    _context.Event.Remove(@event);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
 
         [Authorize]
         [HttpPost]
@@ -259,42 +134,22 @@ namespace DailyApplication.Controllers
             }
             _context.Event.Remove(removableEvent);
 
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(GetUserEvents));
         }
 
-        #endregion
+        #endregion Удалить события
 
-        #region Обновить события
-        // U
-        public void UpdateEvent(Event changedEvent)
-        {
-            var item = _context.Event.Attach(changedEvent);
-            item.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        #region Изменить события
 
-            _context.SaveChanges();
-        }
-        // GET: NewEvents/Edit/5
-        [Authorize]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public void UpdateEvent(Event changedEvent)
+        //{
+        //    var item = _context.Event.Attach(changedEvent);
+        //    item.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
 
-            var @event = await _context.Event.FindAsync(id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
-            return View(@event);
-        }
+        //    _context.SaveChanges();
+        //}
 
-        // POST: NewEvents/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -323,11 +178,11 @@ namespace DailyApplication.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
             }
-            return View(@event);
+            return RedirectToAction(nameof(GetUserEvents));
         }
-        #endregion
+
+        #endregion Изменить события
 
         #region Выполнить событие
 
@@ -340,7 +195,7 @@ namespace DailyApplication.Controllers
             {
                 return NotFound();
             }
-            var processEvent = await _context.Event.FirstOrDefaultAsync(procEv => procEv.Id==id);
+            var processEvent = await _context.Event.FirstOrDefaultAsync(procEv => procEv.Id == id);
             if (processEvent == null)
             {
                 return NotFound();
@@ -351,13 +206,8 @@ namespace DailyApplication.Controllers
             return RedirectToAction(nameof(GetUserEvents));
         }
 
-        #endregion
+        #endregion Выполнить событие
 
-        #region Подробнее о событии 
-
-        #endregion
-
-      
         private bool EventExists(int id)
         {
             return _context.Event.Any(e => e.Id == id);
