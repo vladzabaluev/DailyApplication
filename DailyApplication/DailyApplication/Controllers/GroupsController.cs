1ca0f48c6bae4c161fce2e64cdbf70482b0aabec
awqsomee <awqsome@bk.ru>
2022-06-02 19:07:15 +0300

    Выход из группы

--- DailyApplication/DailyApplication/Controllers/GroupsController.cs
+++ DailyApplication/DailyApplication/Controllers/GroupsController.cs
@@ -146,7 +146,7 @@ namespace DailyApplication.Controllers
 
         public async Task InviteUser(string email, Group group)
         {
-            DailyApplication.Models.User invitedUSer = await _context.User.Where(requiredUser => requiredUser.Email == email).FirstOrDefaultAsync();
+            User invitedUSer = await _context.User.Where(requiredUser => requiredUser.Email == email).FirstOrDefaultAsync();
             if (invitedUSer != null)
             {
                 UserGroup userGroup = await _context.UserGroup.FirstOrDefaultAsync(ug => ug.User == invitedUSer && ug.Group == group);
